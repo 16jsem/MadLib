94f7c3ca8f8fedadcf9d5fdf204613dd1a51d989
Coder <coder@mainlab24.local>
2015-12-17 10:35:29 -0800

    Initial Commit

---
+++ madlib Ethan marx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  madlib Ethan marx
+//
+//  Created by Coder on 12/17/15.
+//  Copyright © 2015 Ethan Marx. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
