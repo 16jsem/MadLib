b899b3edf5efbc46a2b023b4d93ffebe90c68712
Coder <coder@mainlab19.local>
2016-01-06 11:07:59 -0800

    ....

--- MadLib Project/main.swift
+++ MadLib Project/main.swift
@@ -4,8 +4,8 @@
 //
 //  Created by Coder on 1/6/16.
 //  Copyright © 2016 Julianne. All rights reserved.
-//
+// 
+//  CoupevilleLibs
 
 import Foundation
-print("Coupeville Lives")
 print("Hello, World!")
