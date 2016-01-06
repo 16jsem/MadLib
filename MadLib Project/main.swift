3eb1f652b0bda7fa8bbfe54301ae3ead5ebd8457
Coder <coder@mainlab19.local>
2016-01-06 10:32:56 -0800

    Initial Commit

---
+++ MadLib Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MadLib Project
+//
+//  Created by Coder on 1/6/16.
+//  Copyright © 2016 Julianne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
