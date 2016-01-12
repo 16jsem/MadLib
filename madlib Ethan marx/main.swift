58a613d657d1820cc71878c6435d1e045f6a132a
Coder <coder@mainlab24.local>
2016-01-12 10:55:43 -0800

    dfds

--- madlib Ethan marx/main.swift
+++ madlib Ethan marx/main.swift
@@ -43,6 +43,20 @@ var E = input()
 
 
 
+// print
+
+
+
+
+
+
+func input1() -> String {
+    var keyboard = NSFileHandle.fileHandleWithStandardInput()
+    var inputData = keyboard.availableData
+    var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+    
+    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
 
 
 
@@ -50,11 +64,40 @@ var E = input()
 
 
 
+print("Female celeb")
+var madlib5 = input()
+print("'s first husband, the")
+print("adjective")
+var madlib4 = input()
+print("that is")
+print("male celeb")
+var madlib3 = input()
+print("was spotted")
+var madlib2 = input()
+print("adverb")
+print("canoodling with")
+var madlib1 = input()
+print("female celeb")
+print("who he met filming")
+var madlib0 = input ()
+print("name of movie")
+print("in")
+var madlib6 = input()
+print("place")
+print("sadly, the romance ended after")
+var madlib7 = input()
+print("small number")
+print("months, when")
+var madlib8 = input()
+print("same male celeb")
+print("past tense verd")
+var madlib9 = input ()
 
 
 
 
 
 
+//print
 
 
